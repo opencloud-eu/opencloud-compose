68e0e13c2f4301d0e6bf643ef3e04f6768581ed6
Alex Ackermann <abackermann91@gmail.com>
2025-05-30 14:50:42 +0200

    add logo url

---
+++ config/keycloak/themes/opencloud/login/resources/js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const setLogoUrl = (url) => {
+        const logoTextSelector = document.querySelector(".kc-logo-text");
+
+        if (!logoTextSelector) {
+            return
+        }
+
+        const link = document.createElement("a");
+        link.href = url;
+
+        const parent = logoTextSelector.parentNode;
+        parent.insertBefore(link, logoTextSelector);
+        link.appendChild(logoTextSelector);
+    }
+
+    setLogoUrl('https://opencloud.eu')
+});
