c689b262759a46821c9022bc3147ab253fa29cd5
Thomas Schweiger <t.schweiger@opencloud.eu>
2025-10-23 16:03:35 +0200

    fix: change acls and how to apply them

--- config/ldap/init-ldap-acls.sh
+++ config/ldap/init-ldap-acls.sh
@@ -1,26 +1,11 @@
 #!/usr/bin/env bash
 set -eu
 
-# load OpenLDAP environment and functions
-. /opt/bitnami/scripts/libopenldap.sh
-
-trap ldap_stop EXIT
-
-# start LDAP in background
-ldap_start_bg
-
-# wait until LDAP is started
-while ! ldapsearch -Y EXTERNAL -H ldapi:/// -b "cn=admin,dc=opencloud,dc=eu" >/dev/null 2>&1; do
-    echo "Waiting for LDAP to start..."
-    sleep 1
-done
-
 # apply acls
 echo -n "Applying acls... "
-ldapmodify -Y EXTERNAL -H ldapi:/// -f /opt/bitnami/openldap/etc/schema/50_acls.ldif
+slapmodify -F /opt/bitnami/openldap/etc/slapd.d -b cn=config -l /opt/bitnami/openldap/etc/schema/50_acls.ldif
 if [ $? -eq 0 ]; then
     echo "done."
 else
     echo "failed."
 fi
-
